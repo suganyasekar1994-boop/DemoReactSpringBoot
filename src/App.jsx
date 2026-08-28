6b70e05a935d1530e83c00c6a78cce27ebcdf2cf
SuganyaSekar <suganyasekar1994@gmail.com>
2026-08-28 11:15:48 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import ProductManagement from './components/Productmanagement'
+import ProductManagement from './components/ProductManagement.jsx'
 
 
 function App() {
